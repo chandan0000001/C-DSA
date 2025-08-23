dc0262168c5dc513728bfc218f73412cb954413f
CHANDAN <kumardalaichandan803@gmail.com>
2025-08-23 19:36:32 +0530

    Add files via upload
    done

---
+++ DAY01/main.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    cout  << "Hare Krishna  " <<endl;
+    // cout  << " Chandan";
+    // cout  << " Chandan";
+    // cout  << " Chandan";
+    // cout << endl;
+    // cout << "Chandan";
+    // cout << '\n';
+    // cout << "HAttori";
+    // cout << "HAttori" <<endl;
+    // cout << "Dannnnnnnnn" << '\n';
+    // cout << "RAju" <<endl << "Sun" << endl << "GOD";    
+
+
+    // int num = 90;
+    // cout << num <<endl;
+    // cout<< sizeof(num) <<endl;
+
+
+    // char ch = 'j';
+    // cout << ch <<endl;
+    // cout<< sizeof(ch) <<endl;
+
+
+    // float point = 4.6;
+    // cout << point <<endl;
+    // cout << sizeof(point)<<endl;
+
+
+    // long number = 23;
+    // cout << number <<endl;
+    // cout<< sizeof(number) <<endl;
+
+
+    // int num;
+    // cout<< num <<endl;
+
+
+
+    //operators
+    // int a = 3 ;
+    // int b = 2;
+    // cout << a+b <<endl;
+    // cout << a-b <<endl;
+    // cout << a*b <<endl;
+    // cout << a/b <<endl;
+    // cout << a%b <<endl;
+
+
+    //Relational operator 
+    // int f = 6 ;
+    // int g = 7 ;
+    // cout << (f>g) <<endl;
+    // cout << (f<g) <<endl;
+    // cout << (f>=g) <<endl;
+    // cout << (f<=g) <<endl;
+    // cout << (f!=g) <<endl;
+    // cout << (f==g) <<endl;
+
+
+
+
+    // Asignment operator 
+    // int fs = 56;
+
+
+    // //logical operator
+    // int age = 40 ;
+    // int car = 3;
+    // if (age >= 18 && car >=1 ){
+    //     cout << "License mill jjayegi " <<endl;
+    // }
+
+    // cout << !age <<endl;
+
+    // int age1 = 0;
+    // cout << !age1 <<endl;
+                  
+    // int marks ;
+    // cout << "Enter the marls here: " <<endl;
+    // cin >> marks;
+
+    // cout <<"Printing marks " <<marks <<endl;
+
+     char ch;
+     cout << "Enter any character :  ";
+     cin >> ch;
+     cout << "Your character is " <<ch <<endl;
+
+
+
+
+    return 0 ;
+
+}
