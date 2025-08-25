7d636624bf73d59bf7d2a93714fb2e2dbf9c3c56
CHANDAN <kumardalaichandan803@gmail.com>
2025-08-25 21:43:02 +0530

    Add files via upload

---
+++ DAY03/loop.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+
+    //for loop
+    //for (int i = 10 ; i <= 10 ; i++){
+    //}
+
+    // for (int i = 0 ; i <10 ; i++){
+    //     cout << "Chandan is a legend "<<endl;
+    // }
+
+
+    // for ( int j  = 10; j <= 12; j++)
+    // {
+    //     cout <<"Sanskruti is my darlo "<< endl;
+    // }
+    
+
+    // for (int k = 1 ; k <= 10 ; k ++)
+    // {
+    //     cout << k << endl;
+    // }
+
+
+    // for (int k = 1 ; k <= 10 ; k = k + 2)
+    // {
+    //     cout << k << endl;
+    // }
+
+
+    // for (int i = 10 ; i >= 0 ; i--)
+    // {
+    //     cout << i << endl;
+    // }
+
+
+    // for (int i = 0 ; i <5 ; i++){
+    //     cout <<"A" << " ";
+    // }
+    // cout <<endl;
+
+
+    // for (int i = 0 ; i <6 ; i++ ){
+    //     cout<< "Outer Loop " << i << endl  ; 
+    //     for (int j = 0 ; j < 5 ; j++){
+    //         cout<< " Inner Loop  " << j << endl ;
+    //     }
+    // }
+
+    // int i = 1 ;
+
+    // for ( ; ;) {
+    //     cout << "VAlue of I is : " <<i <<endl;
+    //     if (i<5){ 
+    //          cout<< "Chandan" <<endl;
+    //        i++;  
+    //     }
+         
+    // }
+    // cout <<"Hi " <<endl;
+    for (int i = 1 ; i <= 10 ; i++){
+         
+        cout << "19 * " << i << " = " << 19 * i << endl;
+    
+    }
+
+    for (int i = 1 ; i <= 100 ; i++){
+         
+      if ( i % 2 == 0){
+        cout << i << endl;
+      }
+    
+    }
+
+}
---
+++ DAY03/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+   int num;
+   cout << "Enter your number: " ;
+   cin >> num;
+    
+// if ( num == 0) {
+//     cout << "It is not an even or odd it is a zero";
+
+// }else if ( num >= 0 ) {
+
+//     cout << "it is a positive number " <<endl;
+
+// }else if ( num < 0){
+//     cout << "It is a negetive number "<<endl;
+
+// }
+// else {
+//     cout << "It is an imvalid input "<<endl;
+
+
+// if ( num == 0) {
+//     cout << "It is not an even or odd it is a zero";
+
+// }else if ( num %2 == 0 ) {
+
+//     cout << "it is a even number " <<endl;
+
+// }
+// else {
+//     cout << "It is an  odd "<<endl;
+// }
+
+
+}
