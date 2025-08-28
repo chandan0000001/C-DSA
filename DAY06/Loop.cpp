605482119982d5e5bc5b5ce222921b83c6036739
CHANDAN <kumardalaichandan803@gmail.com>
2025-08-28 23:14:28 +0530

    Add files via upload

---
+++ DAY06/Loop.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main ()                                                                                                                               {
+    //while 
+    // int n ; 
+    // cout<<"Enter the number: ";
+    // cin>>n;
+    // int i = 1 ; 
+
+
+
+    // while (i <= 10)
+    // {
+    //     cout<<i*n<<endl;
+    //     i++;
+    // }
+
+
+    // do
+    // {
+    //     cout<<i<<" ";
+    //     i++;
+    // } while (i <= n);
+
+    // int sum = 0 ;
+    // int i = 0;
+    // while (i <= n)
+    // {
+    //     sum = sum + i;
+    //     i++;
+    // }
+    // cout<<sum<<endl;
+
+
+
+
+// int i = 0;
+//     while (i <= 10){
+//         if (i == 4)
+//              {
+//                 break;
+//              }
+//              cout<<i<<" ";
+//              i++;
+//     }
+    
+for (int i = 0 ; i <= 10 ; i++){
+    if (i % 4 == 0 ){
+        continue;
+    }
+    cout<<i<<" ";
+}
+
+
+    
+    
+}
---
+++ DAY06/Operator.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+int main ()                                                                                                                               {
+ //OPerator
+
+
+ //BINARY
+//  int a,b;
+//  cout<<"Enter the first value : "   ;
+//  cin>>a;
+//  cout<<"Enter the second value : "   ;
+//  cin>>b;
+//  int sum = a+b;
+//  int sub  = a-b;
+//  double mul = a*b;
+//  float div  = a/b;
+//  int modulo = a % b ; 
+//  cout<<"YOUR ANSWER IS : "<<endl<<sum<<endl<<sub<<endl<<mul<<endl<<div<<endl<<modulo<<endl;
+
+
+ //unary 
+//  int a = 3;
+//  int b =a++;
+//  cout <<b<<endl;
+//  int v = 8;
+//  int o = ++v;
+//  cout<<o<<endl;
+
+
+ //Comparison operator 
+//  int x = 8 ;
+//  int g = 5 ;
+//  cout<< (x < g) <<endl;
+//  cout<< (x <= g) <<endl;
+//  cout<< (x > g) <<endl;
+//  cout<< (x >= g) <<endl;
+//  cout<< (x == g) <<endl;
+//  cout<< (x != g) <<endl;
+
+
+
+
+
+
+//  cout<< (5>4<6>2<6>0<4<=6>=74==74!=456);
+
+
+
+
+// cout<<(2&&3);
+// cout<<(2&&0);
+// cout<<(2||3);
+// cout<<(2||3);
+
+
+// int a = 7 ;
+// int b = 5 ;
+// int  c = 3;
+
+// if (a>b && a>c){
+//     cout<<a <<"is greater";
+// }else if (b>a && b>c)
+// {
+//        cout<<b<<"is greter" ;
+// }else{
+//     cout<<c<<"is greater";
+// }
+
+
+
+// cout<<(2&3)<<endl;
+// cout<<(2&3)<<endl;
+
+cout<<(7<<2)<<endl;
+cout<<(7>>2)<<endl;
+cout<<(~-7)<<endl;
+
+
+
+}
