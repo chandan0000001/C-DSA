03b66412e348855322e34b53278c21a27af9366c
CHANDAN <kumardalaichandan803@gmail.com>
2025-08-29 21:35:35 +0530

    Add files via upload

---
+++ DAY07/switch.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+   int i ;
+   cout<<"Enter the number : ";
+   cin>>i;
+
+   switch (i)
+   {
+    case 1: 
+    cout<<"Rohit";
+    
+
+    case 2: 
+    cout<<" Monday";
+    break;
+
+
+    case 3 :
+    cout<<"Tuesday";
+    break;
+
+    case 4 :
+    cout<<"Wednesday";
+    break;
+    case 5 :
+    cout<<"Friday";
+    break;
+    case 6 :
+    cout<<"Saturday";
+    break;
+    case 7 :
+    cout<<"Sunday";
+    break;
+
+    default:
+    cout<<"Enter the valid Day";
+   }
+
+
+
+//    char ch ;
+//    cout<<"Enter the character : ";
+//    cin>>ch;
+
+//    switch (ch)
+//    {
+//     case a: 
+//     cout<<"Rohit";
+    
+
+//     case 2: 
+//     cout<<" Monday";
+//     break;
+
+
+//     case 3 :
+//     cout<<"Tuesday";
+//     break;
+
+//     case 4 :
+//     cout<<"Wednesday";
+//     break;
+//     case 5 :
+//     cout<<"Friday";
+//     break;
+//     case 6 :
+//     cout<<"Saturday";
+//     break;
+//     case 7 :
+//     cout<<"Sunday";
+//     break;
+
+//     default:
+//     cout<<"Enter the valid Day";
+//    }
+
+
+}
