b3df3022b59d5de6c4d117fe4f39fd37f4ecdb74
CHANDAN <kumardalaichandan803@gmail.com>
2025-09-25 13:43:41 +0530

    Add files via upload

---
+++ DAY16/Array.cpp
@@ -0,0 +1,40 @@
+// Array which store similar type of an element 
+// only single datat type
+// It sotr in contigious memory
+// We cam aces by throe index no 
+// We have the  power to store multiple value by a single value That is called array
+// int dost[10];  >>Declaraion of array ehich store elements only 10 only declared which store garbage only 
+//  cout<<dost[0] >> shows the fist element 
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+
+//     // declar 
+//     int  number[15];
+//     //Acess array
+//     cout<<number[0];
+//     cout<<endl;
+//     // cout<<number[20];
+//     cout<<"all is well";
+//     cout<<endl;
+//     int second[3]={2,4,6};
+//     cout<<second[2];
+
+//     int third[15]={2,4};
+//     cout<<endl;
+//     // cout<<third[4];
+//    for(int i =0; i <=14; i++){
+//     cout<<third[i]<<endl;   
+// }
+
+//   int fourth[45] = {0};
+//   for (int i =0; i <=44;i++){
+//     cout <<fourth[i]<<endl;
+//   }
+
+
+
+}
